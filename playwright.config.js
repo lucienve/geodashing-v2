b11a1841b5826f188c6507e4496433ed3bd6767b
Lucien Van Elsen <lucienve@gmail.com>
2026-04-08 20:51:50 -0400

    Reduce parallelism on playwright tests and retry potentially flaky failures.

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,8 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  retries: 0,
+  retries: 2,
+  workers: 2,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:8080',
